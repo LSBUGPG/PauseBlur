5154623dc0dd96fb579f65f5e116746b3d101b5a
Paul Sinnett <paul.sinnett@gmail.com>
2020-11-06 13:28:03 +0000

    example
    added animation to the example scene and also froze time when paused

--- Assets/PauseBlur.cs
+++ Assets/PauseBlur.cs
@@ -6,13 +6,16 @@ using UnityEngine.Rendering.PostProcessing;
 public class PauseBlur : MonoBehaviour
 {
     public PostProcessVolume volume;
+    bool paused;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
+            paused = !paused;
             var dof = volume.profile.GetSetting<DepthOfField>();
-            dof.enabled.Override(!dof.enabled);
+            dof.enabled.Override(paused);
+            Time.timeScale = paused? 0.0f : 1.0f;
         }
     }
 }
